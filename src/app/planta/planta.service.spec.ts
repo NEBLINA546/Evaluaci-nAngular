91d969abce267ef9aa890a73ecc33093431696c3
Danna Coral <d.corall@uniandes.edu.co>
2024-04-27 09:03:53 -0500

    Limpiando código no requerido

--- src/app/planta/planta.service.spec.ts
+++ src/app/planta/planta.service.spec.ts
@@ -5,17 +5,14 @@ import { PlantaService } from './planta.service';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { PlantaComponent } from './planta.component';
 import { AppComponent } from '../app.component';
-import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('Service: Planta', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [PlantaService],
-      declarations: [
-        AppComponent, PlantaComponent
-      ],
-    }).compileComponents();
+    });
   });
 
   it('should ...', inject([PlantaService], (service: PlantaService) => {
